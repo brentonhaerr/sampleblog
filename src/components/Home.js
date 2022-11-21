a1a996d0f0c383e5ddc6d72dff90019738f9591e
Brenton Haerr <brenton.haerr@gmail.com>
2022-11-21 14:12:13 -0800

    Tut 10 complete. Added list output.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-  // We have to useState hooks to update reactive values in our component to
-  // make the page re-render.
-  
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = (event) => {
-    setName("Luigi");
-    setAge(age+1);
-  };
-
+  let [blogs, setBlogs] = useState([
+    {title: 'Lorem, ipsum dolor.', body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi soluta, earum quia corrupti velit officiis.", author: "Brenton", id: 1},
+    {title: 'Lorem, ipsum.', body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id odit quis nesciunt at modi deleniti numquam ad obcaecati accusamus! Accusantium explicabo repellat nam.", author: "Brenton", id: 2},
+    {title: 'Lorem ipsum dolor sit.', body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dicta facilis qui sint natus eius consectetur. Ea saepe sint quas eos!", author: "Christy", id: 3},
+  ]);
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <p>Your name: { name }</p>
-      <p>Your age: { age }</p>
-      <button onClick={handleClick}>Click me.</button>
+      { blogs.map((blog)=>(
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+        </div>
+      )) }
     </div>
    );
 }
