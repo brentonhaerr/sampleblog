2c2d25041f766e1b8be8c4395243d7cd599c5992
Brenton Haerr <brenton.haerr@gmail.com>
2022-11-22 12:41:44 -0800

    Tut 20 complete. Custom hook.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,10 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from '../hooks/useFetch';
 
 const Home = () => {
-  let [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-  let [name, setName] = useState("Mario");
 
-  
-
-  useEffect(() => {
-    // You cannot use async inside useEffect unless it is declared within another function. Don't know why! Otherwise you have to use .then/.catch.
-    async function getBlogs() {
-      try {
-        let response = await fetch('http://localhost:8000/blogs');
-        console.log(response);
-        if (!response.ok) throw "Fetch error.";
-        let data = await response.json();
-        console.log(data);
-        setBlogs(data);
-        setError(null);
-        setIsPending(false);
-      } catch (e) {
-        console.log(e);
-        setError(e);
-        setIsPending(false);
-      }
-    }
-
-    setTimeout(getBlogs, 1000);
-  }, []); 
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
   return ( 
     <div className="home">
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+// Custom hooks must start with use.
+/**
+ * @param {string} url 
+ * @returns {} { data, isPending, error }
+ */
+function useFetch(url) {
+  let [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // You cannot use async inside useEffect unless it is declared within another function. Don't know why! Otherwise you have to use .then/.catch.
+    async function getBlogs() {
+      try {
+        let response = await fetch(url);
+        console.log(response);
+        if (!response.ok) throw "Fetch error.";
+        let data = await response.json();
+        console.log(data);
+        setData(data);
+        setError(null);
+        setIsPending(false);
+      } catch (e) {
+        console.log(e);
+        setError(e);
+        setIsPending(false);
+      }
+    }
+
+    setTimeout(getBlogs, 750);
+  }, []); 
+
+  return {data, isPending, error};
+}
+
+export default useFetch;
