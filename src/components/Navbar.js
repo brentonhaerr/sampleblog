f577f6d73c1669ab7c990e5c8de288030c2eff0d
Brenton Haerr <brenton.haerr@gmail.com>
2022-11-22 13:01:50 -0800

    Tut 23 complete. Added Router Links.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return ( 
     <nav className="navbar">
       <h1>DojoBlog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{ 
+        {/* Adding Link tags and the to= value means that the request is handled just via react-router and does not hit the server. Smart! */}
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
           color: "white", 
           backgroundColor: "#f1356d", 
           borderRadius: "8px" 
-        }}>New Blog</a>
+        }}>New Blog</Link>
       </div>
     </nav>
   );
