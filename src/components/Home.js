cb365d16428d4c8bc623daefaa4b3298aef4f017
Brenton Haerr <brentonhaerr@Brentons-Air.lan>
2022-11-22 09:20:20 -0800

    Tut 16 complete. Implemented json server to test api.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  let [blogs, setBlogs] = useState([
-    {title: 'Lorem, ipsum dolor.', body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi soluta, earum quia corrupti velit officiis.", author: "Brenton", id: 1},
-    {title: 'Lorem, ipsum.', body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id odit quis nesciunt at modi deleniti numquam ad obcaecati accusamus! Accusantium explicabo repellat nam.", author: "Brenton", id: 2},
-    {title: 'Lorem ipsum dolor sit.', body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dicta facilis qui sint natus eius consectetur. Ea saepe sint quas eos!", author: "Christy", id: 3},
-  ]);
+  let [blogs, setBlogs] = useState();
 
   let [name, setName] = useState("Mario");
 
@@ -15,21 +11,14 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
-  // Any function in useEffect will trigger when the dom rerenders.
-  // Basically, on first load and when state changes. Either initial
-  // load or when the blogs are deleted, in this case.
-  // You should not generally change state in useEffect because it 
-  // can loop.
   useEffect(()=>{
-    alert("useEffect");
-  // }, []); Adding an empty array here means useEffect only triggers on first load.
-  }, [name]); // Adding a value here means useEffect only triggers on first load AND when that value changes. Only triggers if the value actually changes, not just if it is set to the same value.
+    
+  }, [name]); 
 
   return ( 
     <div className="home">
       <button onClick={()=>(setName("Brenton"))}>Change name</button>
-      <BlogList blogs={ blogs } title="All Blogs" handleDelete={ handleDelete } />
-      <BlogList blogs={ blogs.filter((blog)=>(blog.author=="Brenton")) } title="Brenton Blogs" handleDelete={ handleDelete } />
+     
     </div>
    );
 }
