1e3aa23a3e3c801181d689afcfa10f9754563b66
Brenton Haerr <brenton.haerr@gmail.com>
2022-11-22 13:29:41 -0800

    Tut 26 complete. Reused useFetch hook to get and manipulate data in a different location.

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "../hooks/useFetch";
 
 const BlogDetails = () => {
   // Name the paramter based on the colon format from the router.
   const { id } = useParams();
+  const { data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/'+id);
 
   return ( 
     <div className="blogdetails">
-      <h2>Blog Details - { id }</h2>
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+          <button>Sample Button</button>
+        </article>
+      )}
     </div>
   );
 }
