6de8f6388a20f5118ce84ffee226c523eb22a7da
Brenton Haerr <brentonhaerr@Brentons-Air.lan>
2022-11-22 08:51:13 -0800

    Tut 12 complete. Added filter to component props.

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ const Home = () => {
   return ( 
     <div className="home">
       <BlogList blogs={ blogs } title="All Blogs" />
+      <BlogList blogs={ blogs.filter((blog)=>(blog.author === "Brenton")) } title="Brenton's Blogs" />
     </div>
    );
 }
