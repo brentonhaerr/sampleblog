14202471e49f940a7be1a076e4c8c6f908b1cd35
Brenton Haerr <brentonhaerr@Brentons-Air.lan>
2022-11-22 09:12:13 -0800

    Tut 15 complete. UseEffect dependencies added.

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     {title: 'Lorem ipsum dolor sit.', body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dicta facilis qui sint natus eius consectetur. Ea saepe sint quas eos!", author: "Christy", id: 3},
   ]);
 
+  let [name, setName] = useState("Mario");
+
   const handleDelete = (id) => {
     let newBlogs = blogs.filter((blog) => (blog.id != id));
     setBlogs(newBlogs);
@@ -19,11 +21,13 @@ const Home = () => {
   // You should not generally change state in useEffect because it 
   // can loop.
   useEffect(()=>{
-    
-  });
+    alert("useEffect");
+  // }, []); Adding an empty array here means useEffect only triggers on first load.
+  }, [name]); // Adding a value here means useEffect only triggers on first load AND when that value changes. Only triggers if the value actually changes, not just if it is set to the same value.
 
   return ( 
     <div className="home">
+      <button onClick={()=>(setName("Brenton"))}>Change name</button>
       <BlogList blogs={ blogs } title="All Blogs" handleDelete={ handleDelete } />
       <BlogList blogs={ blogs.filter((blog)=>(blog.author=="Brenton")) } title="Brenton Blogs" handleDelete={ handleDelete } />
     </div>
